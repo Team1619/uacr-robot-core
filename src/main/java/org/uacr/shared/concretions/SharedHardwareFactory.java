90e1e1843284ed3ba14ebe3e7ec329b997acdb5d
Matthew Oates <uni.oates@gmail.com>
2020-06-22 13:17:33 -0600

    Commented HardwareFactory

--- src/main/java/org/uacr/shared/concretions/SharedHardwareFactory.java
+++ src/main/java/org/uacr/shared/concretions/SharedHardwareFactory.java
@@ -9,6 +9,10 @@ import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * Handles the instantiation and storing of external hardware objects
+ */
+
 @Singleton
 public class SharedHardwareFactory implements HardwareFactory {
 
@@ -19,6 +23,14 @@ public class SharedHardwareFactory implements HardwareFactory {
         fObjectMap = new HashMap<>();
     }
 
+    /**
+     * Finds, instantiates, stores, and return external hardware objects for framework hardware objects
+     *
+     * @param tClass the class of the external hardware object to be returned
+     * @param parameters the constructor parameters of the external hardware object to be returned
+     * @param <T> the type of the object to be returned
+     * @return an instance of the requested class
+     */
     public <T> T get(Class<T> tClass, Object... parameters) {
         MultiObjectKey key = createKey(tClass, parameters);
 
@@ -33,6 +45,14 @@ public class SharedHardwareFactory implements HardwareFactory {
         return tObject;
     }
 
+    /**
+     * Instantiates a hardware object of type tClass with the provided parameters
+     *
+     * @param tClass the class to be instantiated
+     * @param parameters the parameters to instantiate the hardware object
+     * @param <T> the type of hardware object to be instantiated
+     * @return the new hardware object
+     */
     private <T> T construct(Class<T> tClass, Object... parameters) {
         Class<?>[] parameterTypes = getParameterTypes(parameters);
         Constructor<?> tConstructor = getConstructor(tClass, parameterTypes);
@@ -47,6 +67,13 @@ public class SharedHardwareFactory implements HardwareFactory {
         }
     }
 
+    /**
+     * Finds the correct constructor of tClass with the provided parameterTypes
+     *
+     * @param tClass the class to find the constructor of
+     * @param parameterTypes the types of the parameters used to find the correct constructor
+     * @return the correct constructor of the object
+     */
     private Constructor<?> getConstructor(Class<?> tClass, Class<?>[] parameterTypes) {
         Constructor<?>[] constructors = tClass.getDeclaredConstructors();
         for (Constructor<?> constructor : constructors) {
@@ -58,18 +85,31 @@ public class SharedHardwareFactory implements HardwareFactory {
         throw new RuntimeException("No constructor found for " + tClass);
     }
 
-    private boolean parameterTypesMatch(Class<?>[] parameterTypes1, Class<?>[] parameterTypes2) {
-        if (parameterTypes1.length != parameterTypes2.length) {
+    /**
+     * Compares two lists of parameter to find the correct constructor of an object
+     *
+     * @param constructorParameterTypes a list of constructor parameters types
+     * @param objectParameterTypes a list of parameters types
+     * @return whether the objectParameterTypes are compatible with the constructorParameterTypes
+     */
+    private boolean parameterTypesMatch(Class<?>[] constructorParameterTypes, Class<?>[] objectParameterTypes) {
+        if (constructorParameterTypes.length != objectParameterTypes.length) {
             return false;
         }
-        for (int p = 0; p < parameterTypes1.length; p++) {
-            if (!convertToNonPrimitiveClass(parameterTypes1[p]).isAssignableFrom(convertToNonPrimitiveClass(parameterTypes2[p]))) {
+        for (int p = 0; p < constructorParameterTypes.length; p++) {
+            if (!convertToNonPrimitiveClass(constructorParameterTypes[p]).isAssignableFrom(convertToNonPrimitiveClass(objectParameterTypes[p]))) {
                 return false;
             }
         }
         return true;
     }
 
+    /**
+     * Checks if a class is primitive and if it is, converts the primitive class to the primitive's wrapper class
+     *
+     * @param tClass the class which could be primitive
+     * @return the original class or a primitive wrapper if the original class was a primitive
+     */
     private Class<?> convertToNonPrimitiveClass(Class<?> tClass) {
         if(tClass == int.class) {
             return Integer.class;
@@ -106,6 +146,12 @@ public class SharedHardwareFactory implements HardwareFactory {
         return tClass;
     }
 
+    /**
+     * Converts an array of parameters to an array of the classes of the original parameters
+     *
+     * @param parameters the parameters to be converted classes
+     * @return an array of classes corresponding to the original parameters
+     */
     private Class<?>[] getParameterTypes(Object[] parameters) {
         Class<?>[] parameterClasses = new Class[parameters.length];
 
@@ -116,6 +162,13 @@ public class SharedHardwareFactory implements HardwareFactory {
         return parameterClasses;
     }
 
+    /**
+     * Creates a MultiObjectKey so hardware objects can be stored and referenced in a Map
+     *
+     * @param tClass the class of the hardware object
+     * @param parameters the parameters of the hardware object
+     * @return a MultiObjectKey corresponding to the class and parameters of the hardware object
+     */
     private MultiObjectKey createKey(Class<?> tClass, Object[] parameters) {
         return new MultiObjectKey(tClass, parameters);
     }
