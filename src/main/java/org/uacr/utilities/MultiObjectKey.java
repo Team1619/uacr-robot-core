ced35b78ec506ca688815dbed8469ae48045430d
Matthew Oates <uni.oates@gmail.com>
2020-07-06 12:23:26 -0600

    Replaced Google Guava with Google Findbugs (#17)

--- src/main/java/org/uacr/utilities/MultiObjectKey.java
+++ src/main/java/org/uacr/utilities/MultiObjectKey.java
@@ -1,20 +1,21 @@
 package org.uacr.utilities;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
 
 public class MultiObjectKey {
 
     private final Object[] fKeys;
 
-    public MultiObjectKey(Object... keys) {
-        if(keys == null) {
+    public MultiObjectKey(@Nullable Object... keys) {
+        if (keys == null) {
             keys = new Object[0];
         }
         fKeys = keys;
     }
 
     public boolean equals(Object object) {
-        if(object instanceof MultiObjectKey) {
+        if (object instanceof MultiObjectKey) {
             return Arrays.deepEquals(fKeys, ((MultiObjectKey) object).fKeys);
         }
         return false;
