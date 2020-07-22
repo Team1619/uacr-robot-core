3fb3fcd5b50279daea1e88a77e6c7a3f24263106
jaseychanders <jasey@chanders.com>
2020-07-21 22:29:51 -0600

    Commented output values

--- src/main/java/org/uacr/shared/concretions/SharedOutputValues.java
+++ src/main/java/org/uacr/shared/concretions/SharedOutputValues.java
@@ -9,6 +9,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Stores the values to be set to all the outputs
+ * Shared between most classes so values can be assigned
+ */
+
 @Singleton
 public class SharedOutputValues implements OutputValues {
 
@@ -18,11 +23,19 @@ public class SharedOutputValues implements OutputValues {
     private final Map<String, Boolean> fOutputBooleans;
     private final Map<String, String> fOutputFlags;
 
+    /**
+     * Creates maps to store the values to be set to each output by output type and value type
+     */
+
     public SharedOutputValues() {
         fOutputNumerics = new ConcurrentHashMap<>();
         fOutputBooleans = new ConcurrentHashMap<>();
         fOutputFlags = new ConcurrentHashMap<>();
     }
+
+    /**
+     * @return a map containing all the outputs (numeric and boolean) and their values
+     */
 
     // All
     @Override
@@ -37,11 +50,23 @@ public class SharedOutputValues implements OutputValues {
         return allOutputs;
     }
 
+    /**
+     * Output flags are used to pass extra information to the outputs
+     * Output flags are cleared after one reading
+     * @param outputName of the output to set the flag on
+     * @param flag the value to be set
+     */
+
     @Override
     public void setOutputFlag(String outputName, String flag) {
         fOutputFlags.put(outputName, flag);
     }
 
+    /**
+     * Output flags are used to pass extra information to the outputs
+     * @param outputName of the output read the flag on
+     * @return the value of the flag after removing the flag from the map
+     */
     @Override
     public String getOutputFlag(String outputName) {
         String flag = fOutputFlags.getOrDefault(outputName, "none");
@@ -50,22 +75,47 @@ public class SharedOutputValues implements OutputValues {
     }
 
 
+    /**
+     * Updates the value of an OutputNumeric
+     * Does not accept a profile string
+     * @param outputNumericName name of the OutputNumeric to be updated
+     * @param outputType type of output such as "percent" mode for a motor
+     * @param outputValue the value to be set
+     */
     // Numeric
     @Override
     public void setNumeric(String outputNumericName, String outputType, double outputValue) {
         setNumeric(outputNumericName, outputType, outputValue, "none");
     }
 
+    /**
+     * Updates the value of an OutputNumeric
+     * Does accept a profile string
+     * @param outputNumericName name of the OutputNumeric to be updated
+     * @param outputType type of output such as "percent" mode for a motor
+     * @param outputValue the value to be set
+     * @param profile a string used to identify the profile used to control the output such as the PID profile for a motor
+     */
+
     @Override
     public void setNumeric(String outputNumericName, String outputType, double outputValue, String profile) {
         fOutputNumerics.put(outputNumericName, Map.of("type", outputType, "value", outputValue, "profile", profile));
     }
+
+    /**
+     * @param outputNumericName name of the OutputNumeric to be read
+     * @return a map containing all of the values of the OutputNumeric
+     */
 
     @Override
     public Map<String, Object> getOutputNumericValue(String outputNumericName) {
         return fOutputNumerics.getOrDefault(outputNumericName, Map.of("value", 0.0, "type", "percent", "profile", "none"));
     }
 
+    /**
+     * @param outputBooleanName the name of the OutputBoolean to have its value updated
+     * @param outputValue the value to update the OutputBoolean to
+     */
 
     // Boolean
     @Override
@@ -73,6 +123,10 @@ public class SharedOutputValues implements OutputValues {
         fOutputBooleans.put(outputBooleanName, outputValue);
     }
 
+    /**
+     * @param outputBooleanName name of the OutputBoolean to be read
+     * @return the value of the OutputBoolean
+     */
     @Override
     public boolean getBoolean(String outputBooleanName) {
         return fOutputBooleans.getOrDefault(outputBooleanName, false);
